012bc23c8e4f728cc880ad55cf278e53acc6b60e
alibek.gao <a.kazhibekov@mail.ru>
2020-11-14 10:57:44 +0200

    users posts to view

--- resources/js/views/Users/Show.vue
+++ resources/js/views/Users/Show.vue
@@ -1,45 +1,69 @@
 <template>
-    <div>
-        <div class="w-100 h-64 overflow-hidden">
-            <img src="https://images.unsplash.com/photo-1605014093414-29b156556886?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80" alt="user bg" class="object-cover w-full">
+    <div class="flex flex-col items-center">
+        <div class="relative mb-8">
+            <div class="w-100 h-64 overflow-hidden z-10">
+                <img src="https://cdn.pixabay.com/photo/2017/03/26/12/13/countryside-2175353_960_720.jpg" alt="user background image" class="object-cover w-full">
+            </div>
+
+            <div class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20">
+                <div class="w-32">
+                    <img src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg" alt="user profile image" class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg">
+                </div>
+
+                <p class="text-2xl text-gray-100 ml-4">{{ user.data.attributes.name }}</p>
+            </div>
         </div>
+
+        <p v-if="postLoading">Loading posts...</p>
+
+        <Post v-else v-for="post in posts.data" :key="post.data.post_id" :post="post" />
+
+        <p v-if=" ! postLoading && posts.data.length < 1">No posts found. Get started...</p>
     </div>
 </template>
 
 <script>
+    import Post from '../../components/Post';
+
     export default {
         name: "Show",
+
+        components: {
+            Post
+        },
 
         data: () => {
             return {
                 user: null,
-                loading: true,
+                posts: null,
+                userLoading: true,
+                postLoading: true,
             }
         },
 
         mounted() {
             axios.get('/api/users/' + this.$route.params.userId)
-                .then( res => {
+                .then(res => {
                     this.user = res.data;
                 })
                 .catch(error => {
-                    console.log('Unable to fetch User');
+                    console.log('Unable to fetch the user from the server.');
                 })
                 .finally(() => {
-                    this.loading = false;
+                    this.userLoading = false;
                 });
-            axios.get('/api/posts/' + this.$route.params.userId)
-                .then( res => {
-                    this.psots = res.data;
+
+            axios.get('/api/users/' + this.$route.params.userId + '/posts')
+                .then(res => {
+                    this.posts = res.data;
                 })
                 .catch(error => {
-                    console.log('Unable to fetch Posts');
+                    console.log('Unable to fetch posts');
                 })
                 .finally(() => {
-                    this.loading = false;
+                    this.postLoading = false;
                 });
         }
-
     }
 </script>
 
